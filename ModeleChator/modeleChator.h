133ec892ed6a16ad68576dae3bda6dda37db068c
bastien.rouiller <bastien.rouiller@gmail.com>
2015-04-14 19:07:33 +0200

    Première version du modèle général Chator

--- ModeleChator/modeleChator.h
+++ ModeleChator/modeleChator.h
@@ -1,48 +1,76 @@
+//Envoyer tous les infos relatives à un user quand il rejoint une salle aux autres utilisateurs, également si ils conaissent déjà l'utilisateur en question
+
 //Contain any informations related to the chat
 class ModelChator
 {
 	private :
-		list<Salle>* salles;
+		list<ModelRoom>* rooms;
 	
 	public :
-		void getRoom(const int idRoom) const;
+		ModelChator();
+	
+		ModeleRoom getRoom(const int idRoom) const;
 		void addRoom(const int idRoom, const Qstring& name, const int limitOfStoredMessage, const bool isPrivate, const bool isVisible, const Qstring& picture);
 		void modifyRoom(const int idRoom, const Qstring& name, const int limitOfStoredMessage, const bool isPrivate, const bool isVisible, const Qstring& picture);
-		void deleteRoom(int idRoom);
-		void addMessage(int idRoom, int idMessage, const QString& contents)
-		void deleteMessage(int idRoom, int idMessage)
+		void deleteRoom(const int idRoom);
+		
+		ModeleMessage getMessage(const idRoom, const idMessage) const;
+		void addMessage(const int idRoom, const int idMessage, const int idUser, const QDate& date, const QString& contents);
+		void modifyMessage(const int idRoom, const int idMessage, const QString& contents);
+		void deleteMessage(const int idRoom, const int idMessage);	
+		
+		ModeleUser getUser(const int idRoom, const int idUser) const;
 }
 
 
 class ModelRoom
 {
 	private :
-		isPrivate;
-		isVisible;
-		list<Users> admins;
-		list<Message> messages;
-		list<User>* users;
+		bool isPrivate;
+		bool isVisible;
+		list<ModelUser>* admins;
+		list<Message>* messages;
+		list<ModelUser>* users;
 	
 	public :
-		addMessage
-		deleteMessage
+		ModelRoom(const int idRoom, const Qstring& name, const int limitOfStoredMessage, const bool isPrivate, const bool isVisible, const Qstring& picture, list<ModelUser>* admins);	
+		ModelRoom(const int idRoom, const Qstring& name, const int limitOfStoredMessage, const bool isPrivate, const bool isVisible, const Qstring& picture, list<ModelUser>* admins, list<ModelUser>* users)
+		~ModelRoom();
+	
+		void modifyRoom(const Qstring& name, const int limitOfStoredMessage, const bool isPrivate, const bool isVisible, const Qstring& picture);
+		void addMessage(const int idMessage, const int idUser, const QDate& date, const QString& contents);
+		void modifyMessage(const int idMessage, const QString& contents);
+		void deleteMessage(const int idMessage);
 }
 
 class ModelMessage
 {
 	private :
-		String user;
-		String message;
-		Date date;
+		int idMessage;
+		int idUser;
+		QDate date;
+		QString contents;
 	
 	public :
-		delete;
+		ModelMessage(const int idMessage, const int idUser, const QDate& date, const QString& contents);
+		~ModelMessage();
+		
+		void modify(const int idMessage, const QString& contents);
 }
 
 class ModelUser
 {
 	private :
+		int idUser;
+		QString userName;
+		QString firstName;
+		QString lastName;
+		bool isConnected;
+		QDate lastConnection;
+		QString image;
 	
 	public :
+		ModelUser(int idUser, QString userName, QString lastName, bool isConnected, QDate lastConnection, QString image);
+		~ModeleUser();
 		
 }
