da82e5d190e7d9626b529c06b0ff927b47fd7990
Benoist Wolleb <planetben1@gmail.com>
2015-04-15 20:22:43 +0200

    les fichiers modeleChator ont été renommés à l'anglaise -> modelChator ajout d'un fichier modelChator.cpp qui contient le corps des méthodes de sérialisation

---
+++ ModeleChator/modelChator.cpp
@@ -0,0 +1,31 @@
+#include "modelChator.h"
+
+QDataStream& operator<< (QDataStream& ds, const ModelRoom& r)
+{
+    
+}
+
+QDataStream& operator>> (QDataStream& ds, ModelRoom& r)
+{
+    
+}
+
+QDataStream& operator<< (QDataStream& ds, const ModelMessage& m)
+{
+    
+}
+
+QDataStream& operator>> (QDataStream& ds, ModelMessage& m)
+{
+    
+}
+
+QDataStream& operator<< (QDataStream& ds, const ModelUser& u)
+{
+    
+}
+
+QDataStream& operator>> (QDataStream& ds, ModelUser& u)
+{
+    
+}
--- Serveur/Interpretor/interpretor.h
+++ Serveur/Interpretor/interpretor.h
@@ -2,8 +2,9 @@
 #define INTERPRETOR_H
 
 #include "modelChator.h"
+#include <QObject>
 
-class Interpretor
+class Interpretor : public QObject
 {
     public:
     void sendMessage(const ModelMessage& message);
