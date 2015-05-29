02f00c4bf6e93b3c641cad7a7027f0944bf6698e
Benoist Wolleb <planetben1@gmail.com>
2015-05-29 09:34:05 +0200

    À l'inscription, les images sont redimensionnées en carré

--- User/viewInscription.cpp
+++ User/viewInscription.cpp
@@ -171,7 +171,7 @@ QImage ViewInscription::getProfileImage() const
 {
     if (!verifyProfileImage()){return QImage();}
     
-    return QImage(ui->ldt_profilPicture->text()).scaledToWidth(PICTURE_WIDTH, Qt::SmoothTransformation);
+    return QImage(ui->ldt_profilPicture->text()).scaled(PICTURE_SIZE, PICTURE_SIZE, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 }
 
 ModelUser* ViewInscription::getCurrentUser()
